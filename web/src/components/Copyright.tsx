65e736fd3e37d972ed4dc5107480caea93067932
Danilo Ferreira <danilof.queiroz1@gmail.com>
2023-05-24 14:57:42 -0300

    feat: create Copyright, EmptyMemories and Hero components

---
+++ web/src/components/Copyright.tsx
@@ -0,0 +1,15 @@
+export function Copyright() {
+  return (
+    <footer className="text-sm leading-none text-gray-200">
+      Feito com 💜 no NLW da{' '}
+      <a
+        target="_blank"
+        rel="noreferrer"
+        href="https://rocketseat.com.br"
+        className="underline hover:text-gray-100"
+      >
+        Rocketseat
+      </a>
+    </footer>
+  )
+}
---
+++ web/src/components/Hero.tsx
@@ -0,0 +1,23 @@
+import Image from 'next/image'
+import nlwLogo from '../assets/nlw-spactetime-logo.svg'
+
+export function Hero() {
+  return (
+    <div className="space-y-5">
+      <Image src={nlwLogo} alt="NLW Spacetime" />
+
+      <div className="max-w-[420px] space-y-1">
+        <h1 className="text-5xl font-bold leading-tight text-gray-50">
+          Sua cápsula do tempo
+        </h1>
+        <p className="text-lg leading-relaxed">
+          Colecione momentos marcantes da sua jornada e compartilhe (se quiser)
+          com o mundo
+        </p>
+      </div>
+      <button className="rounded-full bg-green-500 px-5 py-3 font-alt text-sm font-bold uppercase leading-none text-black hover:bg-green-700">
+        CADASTRAR LEMBRANÇA
+      </button>
+    </div>
+  )
+}
