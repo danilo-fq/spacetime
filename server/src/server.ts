a7bc7343fb51e338e977eeafa467bdac43205fa1
Danilo Ferreira <danilof.queiroz1@gmail.com>
2023-05-25 16:26:31 -0300

    feat: create autentication route returning jwt token

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,10 @@
+import 'dotenv/config'
+
 import fastify from 'fastify'
 import cors from '@fastify/cors'
+import jwt from '@fastify/jwt'
 import { memoriesRoutes } from './routes/memories'
+import { authRoutes } from './routes/auth'
 
 const app = fastify()
 
@@ -8,6 +12,11 @@ app.register(cors, {
   origin: true, // permite todas url's de front end acessar o backend
 })
 
+app.register(jwt, {
+  secret: 'spacetime',
+})
+
+app.register(authRoutes)
 app.register(memoriesRoutes)
 
 app
